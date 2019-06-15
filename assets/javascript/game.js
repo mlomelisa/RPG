22a3dbe445aa65ae7fce6973b979a1b2f2a6bcf3
mlomelisa <monica.lomelim@gmail.com>
2019-06-15 00:03:50 -0700

    Update selection of player, oponent and attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
 
-  let characterObj = [
+  var playerChosen = false;
+  var oponentChosen = false;
+
+  let characterArray = [
     countD = {
       name:"Count Dooku",
       health: 300,
       damage: 40,
       damageChange: 0,
       missChange: 30,
+      
       image: "./assets/images/CountDooku-2.png"},
+      
     anakinS = {
       name:"Anakin Skywalker",
       health: 250,
       damage: 40,
       damageChange: 10,
       missChange:20,
+      
       image: "./assets/images/AnakinSkywalker-2.png"
     },
     yoda = {
-      name:"Grand Master Yoda",
+      name:"Grandmaster Yoda",
       health: 200,
       damage: 25,
       missChange: 0,
@@ -32,18 +38,46 @@ $(document).ready(function() {
       image: "./assets/images/DarthMaul-2.png"
     }
   ]
+   // Generete Attack button
+  function attackButton () {
+    var attackbtn = $('<button>');
+    attackbtn.addClass("btn btn-danger btn-attack").text('Attak');
+    $('#sectionTwo').append(attackbtn);
+    }
 
-
-   for (let i = 0; i < characterObj.length; i++) {
-     console.log(characterObj.length);
-     var charCard = $("<div>").addClass('card');
-     charCard.append($("<h3>").addClass('card-header').text(characterObj[i].name));
-     charCard.append($("<img>").attr('src', characterObj[i].image));
-     charCard.append(($("<div>").addClass('card-body')).append(($('<p>').addClass('card-text')).text(characterObj[i].health)));
-   
+ // Generate the Characters from character Array
+   for (let i = 0; i < characterArray.length; i++) {
+     
+     var charCard = $("<div>").addClass('card cardchar');
+     charCard.append($("<h3>").addClass('card-header').text(characterArray[i].name));
+     charCard.append($("<img>").attr('src', characterArray[i].image));
+     charCard.append(($("<div>").addClass('card-body')).append(($('<p>').addClass('card-text')).text(characterArray[i].health)));
     $('#sectionOne').append(charCard);
 
    }
+  // Move a character when click on it
+
+   $('.cardchar').on('click', function () {
+       if (!playerChosen) {
+        var cardcharSe = $(this).addClass('player');
+        $('#sectionTwo').append(cardcharSe);
+        playerChosen = true;
+        attackButton();
+       } else {
+         if(!oponentChosen) {
+          var cardcharSe = $(this).addClass('oponent');
+          $('#sectionTwo').append(cardcharSe);
+          oponentChosen = true;
+         } else{
+           return false;
+         }
+              
+       }
+  });
+
+ 
 
 
-});
+
+}); // Ready
+
